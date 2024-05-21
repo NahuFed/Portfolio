532ae5b51f43e5bbb4e00e9d151f78cc0c198c30
Nahuel Rodriguez <nahufed@gmail.com>
2024-05-20 22:35:50 -0300

    agregada estructura proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h3>Header</h3>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/main.css'
+
+const Main = () =>{
+    return (
+        <main>
+            <h3>Main</h3>
+        </main>
+    )
+}
+
+export default Main;
