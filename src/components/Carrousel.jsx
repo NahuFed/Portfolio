13193a17b14025f26499151aef63cdc51a2af4d6
Nahuel Rodriguez <nahufed@gmail.com>
2024-05-21 20:40:08 -0300

    se instalo fontawesome para react, se agregaron estilos sidebar y body

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -4,35 +4,35 @@ import img1 from '../assets/img1.jpg'
 import img2 from '../assets/img2.jpg'
 import img3 from '../assets/img3.jpg'
 import { Image } from 'react-bootstrap';
-
+import '../css/carousel.css'
 
 const Carrousel = () => {
     return (
-        <Carousel>
-      <Carousel.Item className='carousel-funky-img'>
+      <div className='contenedor-relativo'>
+      <h1 className='nombre'>NAHUEL RODRIGUEZ</h1>
+      <Carousel controls={false} slide={true}>          
+          
+      <Carousel.Item className='carousel-funky-img' interval={5000}>
         <Image src={img1}/>
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
+          
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item className='carousel-funky-img'>
+      <Carousel.Item className='carousel-funky-img' interval={5000}>
       <Image src={img2}/>
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item className='carousel-funky-img'>
+      <Carousel.Item className='carousel-funky-img' interval={5000}>
       <Image src={img3}/>
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+      </div>
     );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,175 @@
 import React from "react";
 import "../css/main.css";
 import Carrousel from "./Carrousel";
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faHtml5,faReact,faCss3,faJs } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase,faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
   return (
     <main>
       <Carrousel />
 
-      <div id="acercademi" className="w-25">
-        <h3>aca dice todo acerca de mi</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum expedita eius, aspernatur vitae quo commodi alias id atque, necessitatibus ea praesentium reiciendis culpa vero voluptatem eos ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis nemo animi assumenda! Provident molestiae reiciendis quasi ut velit fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus similique? Explicabo quos dolore minus vitae temporibus earum, voluptas iusto in quas quod suscipit aut, repellendus culpa dolores! Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore. Ex, quibusdam!</p>
+      <div id="acercademi">
+        <div className="w-25 informacion">
+          <h3>ACERCA DE MI</h3>
+          <p>
+            Hola, soy un desarollador Frontend de Tucumán, Argentina. Estoy
+            cursando el ultimo año de la tecnicatura universitaria en
+            programación en la Universidad Tecnológica Nacional - FRT
+          </p>
+          <ul className="datospersonales">
+            <li>
+              <span className="texto">Nombre:</span>Nahuel Rodriguez
+            </li>
+            <li>
+              <span className="texto">Email:</span>nahufed@gmail.com
+            </li>
+            <li>
+              <span className="texto">Telefono:</span>(381)-620-5792
+            </li>
+            <li>
+              <span className="texto">Nacionalidad:</span>Argentina
+            </li>
+          </ul>
+          <a className="btn btn-dark rounded-0 p-3 mt-5" href="#">
+            DESCARGAR CURRICULUM
+          </a>
+        </div>
       </div>
-      <div id="skills" className="w-25">
-        <h3>aca dice todo de mis skills</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum expedita eius, aspernatur vitae quo commodi alias id atque, necessitatibus ea praesentium reiciendis culpa vero voluptatem eos ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis nemo animi assumenda! Provident molestiae reiciendis quasi ut velit fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus similique? Explicabo quos dolore minus vitae temporibus earum, voluptas iusto in quas quod suscipit aut, repellendus culpa dolores! Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore. Ex, quibusdam!</p>
+      <div id="skills">
+        <h3>SKILLS</h3>
+        <div className="row p-5 g-5">
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faHtml5} className="fs-1 me-4 text-danger"/>
+            </div>
+            <div>
+              <h5>HTML 5</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faCss3} className="fs-1 me-4 text-primary"/>
+            </div>
+            <div>
+              <h5>CSS 3</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faJs} className="fs-1 me-4 text-warning"/>
+            </div>
+            <div>
+              <h5>JAVASCRIPT</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faReact} className="fs-1 me-4 text-info"/>
+            </div>
+            <div>
+              <h5>REACT</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faDatabase} className="fs-1 me-4"/>
+            </div>
+            <div>
+              <h5>SQL</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+          <div className="col-4 d-flex">
+            <div>
+            <FontAwesomeIcon icon={faPeopleGroup} className="fs-1 me-4"/>
+            </div>
+            <div>
+              <h5>SCRUM</h5>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+                deleniti magni, quos laborum iure quod quia architecto fugit
+                commodi doloribus.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div id="educacion" className="w-25">
-        <h3>aca dice todo de mi educacion</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum expedita eius, aspernatur vitae quo commodi alias id atque, necessitatibus ea praesentium reiciendis culpa vero voluptatem eos ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis nemo animi assumenda! Provident molestiae reiciendis quasi ut velit fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus similique? Explicabo quos dolore minus vitae temporibus earum, voluptas iusto in quas quod suscipit aut, repellendus culpa dolores! Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore. Ex, quibusdam!</p>
+      <div id="educacion">
+        <h3>EDUCACION</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          expedita eius, aspernatur vitae quo commodi alias id atque,
+          necessitatibus ea praesentium reiciendis culpa vero voluptatem eos
+          ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis
+          nemo animi assumenda! Provident molestiae reiciendis quasi ut velit
+          fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente
+          optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus
+          similique? Explicabo quos dolore minus vitae temporibus earum,
+          voluptas iusto in quas quod suscipit aut, repellendus culpa dolores!
+          Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem
+          numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore.
+          Ex, quibusdam!
+        </p>
       </div>
-      <div id="portfolio" className="w-25">
-        <h3>aca dice todo de mi portfolio</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum expedita eius, aspernatur vitae quo commodi alias id atque, necessitatibus ea praesentium reiciendis culpa vero voluptatem eos ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis nemo animi assumenda! Provident molestiae reiciendis quasi ut velit fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus similique? Explicabo quos dolore minus vitae temporibus earum, voluptas iusto in quas quod suscipit aut, repellendus culpa dolores! Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore. Ex, quibusdam!</p>
+      <div id="portfolio">
+        <h3>PORTFOLIO</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          expedita eius, aspernatur vitae quo commodi alias id atque,
+          necessitatibus ea praesentium reiciendis culpa vero voluptatem eos
+          ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis
+          nemo animi assumenda! Provident molestiae reiciendis quasi ut velit
+          fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente
+          optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus
+          similique? Explicabo quos dolore minus vitae temporibus earum,
+          voluptas iusto in quas quod suscipit aut, repellendus culpa dolores!
+          Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem
+          numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore.
+          Ex, quibusdam!
+        </p>
       </div>
-      <div id="contacto" className="w-25">
-        <h3>aca dice todo de mi contacto</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum expedita eius, aspernatur vitae quo commodi alias id atque, necessitatibus ea praesentium reiciendis culpa vero voluptatem eos ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis nemo animi assumenda! Provident molestiae reiciendis quasi ut velit fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus similique? Explicabo quos dolore minus vitae temporibus earum, voluptas iusto in quas quod suscipit aut, repellendus culpa dolores! Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore. Ex, quibusdam!</p>
+      <div id="contacto">
+        <h3>CONTACTO</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          expedita eius, aspernatur vitae quo commodi alias id atque,
+          necessitatibus ea praesentium reiciendis culpa vero voluptatem eos
+          ducimus quia asperiores ipsam eligendi blanditiis! Cum dicta officiis
+          nemo animi assumenda! Provident molestiae reiciendis quasi ut velit
+          fugit, aspernatur dignissimos vero laborum debitis suscipit sapiente
+          optio totam, quibusdam pariatur eum necessitatibus ipsam voluptatibus
+          similique? Explicabo quos dolore minus vitae temporibus earum,
+          voluptas iusto in quas quod suscipit aut, repellendus culpa dolores!
+          Dolor, aut. Ad aliquam quaerat nam aliquid, alias odit incidunt quidem
+          numquam reprehenderit facere obcaecati ipsam, eos ab sequi inventore.
+          Ex, quibusdam!
+        </p>
       </div>
     </main>
   );
